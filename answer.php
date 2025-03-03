d8a80175fdb643c6bf595f15d82a6d1fe4b009aa
default <yunus.emre.guzel@mths.ca>
2025-03-03 16:55:29 +0000

    added the new files

--- answer.php
+++ answer.php
@@ -22,25 +22,24 @@
   <body>
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-    <header class="mdl-layout__header">
-      <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Calculate</span>
-      </div>
-    </header>
-    <br>
-    <form class='clauclator-form'  action="answer.php" method="GET">
+    <form class='clauclator-form' action="#">
       <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
         <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="sample4">
         <label class="mdl-textfield__label" for="sample4">Number...</label>
         <span class="mdl-textfield__error">Input is not a number!</span>
       </div>
     </form>
-    <main id="click me " class="mdl-layout__content">
-      <button onclick="Helloworld()" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
-        Answer
-      </button>
-    </main>
+    <main class="mdl-layout__content">
+        <div class="right-image">
+          <img src="./" alt="laptop image" />
+        </div>
+        <div class="page-content">The answer is ...</div>
+        <br />
+        <div class="page-content-answer">
+          <a href="./index.php">Return ...</a>
+        </div>
+      </main>
   </div>
-    <img class="Calculate" src="images/clauclator.jpg" alt="">
+    <?php echo '<p>Hello, World!, in PHP</p>'; ?>
   </body>
 </html>
