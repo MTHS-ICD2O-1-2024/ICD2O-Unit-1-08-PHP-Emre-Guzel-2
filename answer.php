7af18bb7256dabc0fa59dd4d122135fac0705577
default <yunus.emre.guzel@mths.ca>
2025-03-03 21:37:27 +0000

    changed the asswer .php

--- answer.php
+++ answer.php
@@ -22,23 +22,40 @@
   <body>
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-    <form class='clauclator-form' action="#">
-      <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-        <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="sample4">
-        <label class="mdl-textfield__label" for="sample4">Number...</label>
-        <span class="mdl-textfield__error">Input is not a number!</span>
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">Calculate</span>
+        </header>
       </div>
-    </form>
-    <main class="mdl-layout__content">
+        <form action="answer.php" method="GET">
+          <div class="mdl-textfield mdl-js-textfield">
+            <!-- input pattern attribute -->
+            <input
+              class="mdl-textfield__input"
+              type="text"
+              pattern="-?[0-9]*(\.[0-9]+)?"
+              id="length"
+            />
+            <!-- mdl-textfield__label -->
+            <label class="mdl-textfield__label" for="demo-input"
+              >Length of ... (mm)</label
+            >
+            <!-- class "mdl-textfield__error" -->
+            <span class="mdl-textfield__error">Input is not a number</span>
+          <br />
+          <!-- Accent-colored raised button with ripple -->
+          <br> <br>
+          <main class="mdl-layout__content">
         <div class="right-image">
           <img src="./" alt="laptop image" />
         </div>
         <br />
-        <?php echo '<p>Hello, World!, in PHP</p>'; ?>
         <div class="page-content-answer">
           <a href="./index.php">Return ...</a>
         </div>
+        <echo>
       </main>
-  </div>
+        </form>
+        <?php echo '<p>Hello, World! </p>'; ?>
   </body>
-</html> 
+</html>
