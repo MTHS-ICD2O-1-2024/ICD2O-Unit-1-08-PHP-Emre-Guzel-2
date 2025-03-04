df2d05d1614d8254c97295c46fa895df49db651a
default <yunus.emre.guzel@mths.ca>
2025-03-04 18:15:06 +0000

    fix the erro ins php

--- index.php
+++ index.php
@@ -22,24 +22,47 @@
 </head>
 
 <body>
-  <script defer src="https://code.getmdl.io/1.3.0/material.min.js""></script>
-  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-    <header class="mdl-layout__header">
-      <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Calculate</span>
-      </div>
-    </header>
-    <br>
-      <form action="answer.php" method="GET">
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+      <header class="mdl-layout__header">
+        <div class="mdl-layout__header-row">
+          <span class="mdl-layout-title">The XXX program, PHP</span>
+        </div>
+      </header>
+      <main class="mdl-layout__content">
+        <div class="right-image">
+          <img class="Calculate" src="./images/clauclator.jpg" alt="laptop image" />
+        </div>
+        <div class="page-content">Instructions here ...</div>
         <br />
-        <!-- Accent-colored raised button with ripple -->
-        <button
-          class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-          type="submit">
-          answer
-        </button>
-    </main>
-  </form>
-    <img class="Calculate" src="./images/clauclator.jpg" alt="">
+        <!-- Simple Textfield for integers-->
+        <form action="answer.php" method="GET">
+          <div class="mdl-textfield mdl-js-textfield">
+            <!-- input pattern attribute -->
+            <input
+              class="mdl-textfield__input"
+              type="text"
+              pattern="-?[0-9]*(\.[0-9]+)?"
+              id="length"
+            />
+            <!-- mdl-textfield__label -->
+            <label class="mdl-textfield__label" for="demo-input"
+              >Length of ... (mm)</label
+            >
+            <!-- class "mdl-textfield__error" -->
+            <span class="mdl-textfield__error">Input is not a number</span>
+          </div>
+          <br />
+          <!-- Accent-colored raised button with ripple -->
+          <button
+            class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
+            type="submit"
+          >
+            Calculate
+          </button>
+        </form>
+        <br />
+      </main>
+    </div>
 </body>
 </html>
